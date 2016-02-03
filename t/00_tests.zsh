c283a59a36ae11f66f19a0b451ccce792fdcf428
Marc Chantreux <khatar@phear.org>
2016-02-03 17:41:54 +0100

    start a test suite

---
+++ t/00_tests.zsh
@@ -0,0 +1,12 @@
+shush uze devel/TAP :all ||exit
+
+does_return_works () {
+    local cmd ret
+    for cmd ret (
+        true  0
+        false 1
+    ) { $cmd
+        does_return $ret "$cmd is $cmd" }
+}
+
+prove does_return_works
