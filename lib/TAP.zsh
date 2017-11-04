ac5be577010a7cac2f0c44e709d14925cb8552cb
Marc Chantreux <mc@unistra.fr>
2017-11-04 09:38:45 +0100

    remove devel/ from the namesapace
    as TAP is usefull in many places

--- lib/TAP.zsh
+++ lib/TAP.zsh
@@ -1,4 +1,4 @@
-devel/TAP/prove () {
+TAP/prove () {
     typeset -A with
     while [[ $argv[1] = :* ]] { with+=( $argv[1,2] ); shift 2 }
 
@@ -12,7 +12,7 @@ devel/TAP/prove () {
 
 }
 
-devel/TAP/ok () {
+TAP/ok () {
     local _tap_test_r=$?
     if (( !_tap_test_r )) {
         print "ok $[++_test_tap_index] $1"
@@ -22,31 +22,31 @@ devel/TAP/ok () {
     return $_tap_test_r
 }
 
-devel/TAP/not_ok () {
+TAP/not_ok () {
     local r=$?
     ! (( $? )); ok "$@"
     return $r
 }
 
-devel/TAP/note () {
+TAP/note () {
     local r=$?
     print "# $*"
     return $r
 }
 
-devel/TAP/note- () {
+TAP/note- () {
     local r=$?
     sed 's/^/# /'
     print "# $*"
     return $r
 }
 
-devel/TAP/is   () {   [[ "$1" == "$2" ]]; devel/TAP/ok "$3" }
-devel/TAP/isnt () { ! [[ "$1" == "$2" ]]; devel/TAP/ok "$3" }
-devel/TAP/expected   () { devel/TAP/is   "$got" "$expected" "$1" }
-devel/TAP/unexpected () { devel/TAP/isnt "$got" "$expected" "$1" }
+TAP/is   () {   [[ "$1" == "$2" ]]; TAP/ok "$3" }
+TAP/isnt () { ! [[ "$1" == "$2" ]]; TAP/ok "$3" }
+TAP/expected   () { TAP/is   "$got" "$expected" "$1" }
+TAP/unexpected () { TAP/isnt "$got" "$expected" "$1" }
 
-uze/export/devel/TAP () {
+uze/export/TAP () {
     delegate=true
     EXPORT_TAGS=( :all 'ok not_ok prove is note expected diag' )
 }
